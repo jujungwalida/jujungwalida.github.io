fd14cb59eb9bab2dd49ce108bd9e4fd612ddaaba
Jujung Walida <jujungwalida@gmail.com>
2021-07-23 22:27:48 +0700

    Fix everything

--- README.md
+++
@@ -1,4 +0,0 @@
-# PT Nada Salama International
-
-- [Desain](https://jujungwalida.github.io/theme/v-1.1/home.html).
-- [Full Source Code](https://github.com/jujungwalida/jujungwalida.github.io).
--- wp-content/themes/nadasalama/footer.php
+++
@@ -1,9 +0,0 @@
-
-		<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
-
-		<?php get_template_part( 'template-parts/footer/footer-copyright' ); ?>
-
-		<?php wp_footer(); ?>
-
-	</body>
-</html>
